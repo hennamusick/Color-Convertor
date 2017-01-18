66d9b0adfde11283eb900c008973c57428e27c34
ChathuraHennayaka <hennayaka121@gmail.com>
2017-01-18 15:00:45 +0530

    HTTP API test included to the server.js

---
+++ test/server.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var request = require("request");
+
+describe('Color code convertor API', function() {
+    describe('RGB to HEX conversion', function() {
+
+        it('returns status 200', function() {
+        });
+
+        it('retruns the Color in hex', function() {
+        });
+    });
+
+    describe('HEX to RGB conversion', function() {
+        
+        it('returns status 200', function() {
+        });
+
+        it('retruns the Color in hex', function() {
+        });
+    });
+});
