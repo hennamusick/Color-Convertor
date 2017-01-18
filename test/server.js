965b8ab1a4638ec158bc14f953c96fdea430c381
ChathuraHennayaka <hennayaka121@gmail.com>
2017-01-18 15:16:58 +0530

    Added url into the test/server.js

--- test/server.js
+++ test/server.js
@@ -4,19 +4,17 @@ var request = require("request");
 describe('Color code convertor API', function() {
     describe('RGB to HEX conversion', function() {
 
-        it('returns status 200', function() {
-        });
+        var url = "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
 
-        it('retruns the Color in hex', function() {
-        });
+        it('returns status 200', function() {});
+        it('retruns the Color in hex', function() {});
     });
 
     describe('HEX to RGB conversion', function() {
-        
-        it('returns status 200', function() {
-        });
 
-        it('retruns the Color in hex', function() {
-        });
+        var url = "http://localhost:3000/hexToRgb?hex=00ff00";
+
+        it('returns status 200', function() {});
+        it('retruns the Color in hex', function() {});
     });
 });
