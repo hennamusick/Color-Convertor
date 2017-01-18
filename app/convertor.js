671c6a72cc63bec004d7b8797159478e7587c92d
ChathuraHennayaka <hennayaka121@gmail.com>
2017-01-18 14:23:04 +0530

    completed app/convertor.js

--- app/convertor.js
+++ app/convertor.js
@@ -7,3 +7,15 @@ exports.rgbToHex = function(red, green, blue) {
     return pad(redHex) + pad(greenHex) + pad(blueHex);
 };
 
+function pad(hex) {
+    return (hex.legth === 1 ? "0" + hex : hex);
+}
+
+exports.hexToRgb = function(hex) {
+
+    var red = parseInt(hex.substring(0,2),16);
+    var green = parseInt(hex.substring(2,4),16);
+    var blue = parseInt(hex.substring(4,6),16);
+
+    return [red,green,blue];
+}
--- test/convertor.js
+++ test/convertor.js
@@ -3,23 +3,23 @@ var expect = require('chai').expect;
 var convertor = require('../app/convertor');
 
 describe('Color convertor', function() {
-    describe('RGB to HEX conversion', function() {
+    describe('RGB to HEX conversion\n', function() {
         it('Coverts the basic Color', function() {
             var redHex = convertor.rgbToHex(255,0,0);
             var greenHex = convertor.rgbToHex(0,255,0);
             var blueHex = convertor.rgbToHex(0,0,255);
 
-            expect(redHex).to.equal("ffoooo");
-            expect(greenHex).to.equal("ooffoo");
-            expect(blueHex).to.equal("ooooff");
+            expect(redHex).to.equal("ff00");
+            expect(greenHex).to.equal("0ff0");
+            expect(blueHex).to.equal("00ff");
         });
     });
 
-    describe('HEX to RGB conversion', function() {
+    describe('HEX to RGB conversion\n', function() {
         it('Converts the basic Color', function() {
-            var red = convertor.hexToRgb('ffoooo');
-            var green = convertor.hexToRgb('ooffoo');
-            var blue = convertor.hexToRgb('ooooff');
+            var red = convertor.hexToRgb('ff0000');
+            var green = convertor.hexToRgb('00ff00');
+            var blue = convertor.hexToRgb('0000ff');
 
             expect(red).to.deep.equal([255,0,0]);
             expect(green).to.deep.equal([0,255,0]);
