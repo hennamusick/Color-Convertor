9443c2fa7f439ff2038ac0321be72f1074d78026
ChathuraHennayaka <hennayaka121@gmail.com>
2017-01-18 11:16:39 +0530

    added a description

--- test/convertor.js
+++ test/convertor.js
@@ -1,7 +1,13 @@
 describe('Color convertor', function() {
     describe('RGB to HEX conversion', function() {
+        it('Coverts the basic Color', function() {
+
+        });
     });
 
     describe('HEX to RGB conversion', function() {
+        it('Converts the basic Color', function() {
+
+        });
     });
 });
